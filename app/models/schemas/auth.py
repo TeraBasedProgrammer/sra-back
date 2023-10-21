f548de85e1a0bf511a6bd74a3ba8c51d191474c7
Ilya <ilyadronov2332@gmail.com>
2023-10-21 14:20:10 +0300

    feat: added database config file; added user and auth pydantic schemas

---
+++ app/models/schemas/auth.py
@@ -0,0 +1,30 @@
+import re
+from typing import Optional
+
+from fastapi import HTTPException
+from pydantic import BaseModel, EmailStr, field_validator
+from starlette import status
+
+from app.config.logs.logger import logger
+from app.models.schemas.users import UserBase
+
+
+class UserSignUp(UserBase):
+    password: str
+    name: Optional[str] = None
+
+    @field_validator("password")
+    def validate_password(cls, value):
+        if not re.compile(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$").match(value):
+            logger.warning(f"Validation error: password doesn't match the pattern")
+            raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Password should contain at least eight characters, at least one letter and one number")
+        return value
+
+
+class UserSignUpAuth0(UserSignUp):
+    auth0_registered: bool
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str

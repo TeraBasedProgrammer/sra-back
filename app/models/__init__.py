66c18eb2d3f4e91646b090a8a635aa34bd55088d
Ilya <ilyadronov2332@gmail.com>
2023-10-26 17:49:59 +0300

    fix(db): imported all sqlalchemy classes to __init__.py to avoid mapper error

---
+++ app/models/__init__.py
@@ -0,0 +1,4 @@
+from app.models.db.attempts import Attempt
+from app.models.db.companies import Company, CompanyUser
+from app.models.db.users import User, TagQuiz, TagUser, Tag
+from app.models.db.quizzes import Question, Quiz, Answer

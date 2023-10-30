280e4b679e4dfab4b742e2a290229d647e3159f9
Ilya <ilyadronov2332@gmail.com>
2023-10-30 23:50:26 +0200

    feat: added reset password endpoint

--- app/api/docs/user_profile.py
+++ app/api/docs/user_profile.py
@@ -4,15 +4,60 @@ from fastapi import status
 
 
 def get_profile_responses() -> dict[int, Any]:
-    responses: dict[int, Any] = (
-        {
-            status.HTTP_401_UNAUTHORIZED: {
-                "description": "Token decode error or token was not provided",
-                "content": {
-                    "application/json": {"example": {"detail": "Not authenticated"}}
-                },
+    responses: dict[int, Any] = {
+        status.HTTP_401_UNAUTHORIZED: {
+            "description": "Token decode error or token was not provided",
+            "content": {
+                "application/json": {"example": {"detail": "Not authenticated"}}
             },
-        }
-    )
+        },
+    }
+
+    return responses
+
+
+def get_reset_password_responses() -> dict[int, Any]:
+    responses: dict[int, Any] = {
+        status.HTTP_401_UNAUTHORIZED: {
+            "description": "Token decode error or token was not provided",
+            "content": {
+                "application/json": {"example": {"detail": "Not authenticated"}}
+            },
+        },
+        status.HTTP_400_BAD_REQUEST: {
+            "description": "Invalid old password",
+            "content": {
+                "application/json": {"example": {"detail": "Invalid pld password"}}
+            },
+        },
+        status.HTTP_409_CONFLICT: {
+            "description": "The new password matches the old one",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "Matching passwords"
+                    }
+                }
+            },
+        },
+        status.HTTP_422_UNPROCESSABLE_ENTITY: {
+            "description": "One or more fields were passed incorrectly",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": [
+                            {
+                                "type": "string_type",
+                                "loc": ["body", "old_password"],
+                                "msg": "Input should be a valid string",
+                                "input": 0,
+                                "url": "https://errors.pydantic.dev/2.1.2/v/string_type",
+                            }
+                        ]
+                    }
+                }
+            },
+        },
+    }
 
     return responses
--- app/api/routes/user_profile.py
+++ app/api/routes/user_profile.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends
 
 from app.api.dependencies.services import get_user_service
 from app.api.dependencies.user import get_current_user
+from app.api.docs.user_profile import (get_profile_responses,
+                                       get_reset_password_responses)
 from app.models.schemas.company_user import UserFullSchema
+from app.models.schemas.users import PasswordResetInput, PasswordResetOutput
 from app.services.user import UserService
-from app.api.docs.user_profile import get_profile_responses
 
 router = APIRouter(
     prefix="/profile",
@@ -18,8 +20,30 @@ router = APIRouter(
     response_model_exclude_none=True,
     responses=get_profile_responses(),
 )
-async def get_current_user(
+async def get_user_profile(
     current_user: int = Depends(get_current_user),
     user_service: UserService = Depends(get_user_service),
 ) -> UserFullSchema:
     return await user_service.get_user_profile(current_user)
+
+
+@router.patch("/edit", response_model=None)
+async def edit_user_profile(
+    data: None,
+    current_user: int = Depends(get_current_user),
+    user_service: UserService = Depends(get_user_service),
+) -> None:
+    return await user_service.update_user_profile(current_user)
+
+
+@router.patch(
+    "/edit/reset_password",
+    response_model=PasswordResetOutput,
+    responses=get_reset_password_responses(),
+)
+async def reset_password(
+    data: PasswordResetInput,
+    current_user: int = Depends(get_current_user),
+    user_service: UserService = Depends(get_user_service),
+) -> PasswordResetOutput:
+    return await user_service.reset_password(current_user, data)

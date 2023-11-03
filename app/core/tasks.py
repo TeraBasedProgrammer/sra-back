0e124c060497e3f09f29fd8e24fd6582f989d1ad
Ilya <ilyadronov2332@gmail.com>
2023-11-03 14:56:06 +0200

    feat(background tasks): added background task to send email with reset link

---
+++ app/core/tasks.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.message import EmailMessage
+
+from celery import Celery
+from pydantic import EmailStr
+
+from app.config.settings.base import settings
+
+celery = Celery("tasks", broker=settings.REDIS_URL)
+
+
+def get_email_template_dashboard(user_email: EmailStr, user_name: str, reset_link: str):
+    email = EmailMessage()
+    email["Subject"] = "Password reset request"
+    email["From"] = settings.SMTP_USER
+    email["To"] = user_email
+
+    email.set_content(
+        f'<div style="color: blue;">{user_name} {reset_link}</div>', subtype="html"
+    )
+    return email
+
+
+@celery.task
+def send_email_report_dashboard(user_email: EmailStr, user_name: str, reset_link: str):
+    email = get_email_template_dashboard(user_email, user_name, reset_link)
+    with smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_PORT) as server:
+        server.login(settings.SMTP_USER, settings.SMTP_PASSWORD)
+        server.send_message(email)

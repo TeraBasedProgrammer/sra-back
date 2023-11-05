5c255a9d3e2d9f5b71ece1bee190c6f2eed659de
Ilya <ilyadronov2332@gmail.com>
2023-11-05 23:41:44 +0200

    refactor: moved tag crud operations under company router

--- app/api/routes/companies.py
+++ app/api/routes/companies.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 
 from app.api.dependencies.auth import auth_wrapper
-from app.api.dependencies.services import get_company_service
-from app.api.dependencies.user import get_current_user
+from app.api.dependencies.services import get_company_service, get_tag_service
+from app.api.dependencies.user import get_current_user, get_current_user_id
 from app.api.docs.companies import (
     get_create_company_responses,
     get_get_company_responses,
@@ -12,6 +12,7 @@ from app.models.schemas.companies import CompanyCreate, CompanyCreateSuccess
 from app.models.schemas.company_user import CompanyFullSchema
 from app.models.schemas.tags import TagBaseSchema
 from app.services.company import CompanyService
+from app.services.tag import TagService
 
 router = APIRouter(
     prefix="/companies",
@@ -36,17 +37,22 @@ async def get_company(
     return await company_service.get_company_by_id(company_id)
 
 
-# 200, 400, 403, 401, 422
+# TODO 200, 400, 403, 401, 422
 @router.get("/{company_id}/tags/", response_model=list[TagBaseSchema])
 async def get_tags_list(
     company_id: int,
-    current_user: User = Depends(get_current_user),
-    company_service: CompanyService = Depends(get_company_service),
+    current_user_id: int = Depends(get_current_user_id),
+    tag_service: TagService = Depends(get_tag_service),
 ) -> list[TagBaseSchema]:
     """
     ### Returns a list with all the available tags within the company
     """
-    return await company_service.get_company_tags(current_user, company_id)
+    return await tag_service.get_company_tags(current_user_id, company_id)
+
+
+@router.get("/{company_id}/tags/{tag_id}/")
+async def get_tag(tag_id: int):
+    pass
 
 
 @router.post("/create/", status_code=201, responses=get_create_company_responses())
@@ -59,3 +65,18 @@ async def create_company(
     ### Create a new company instance
     """
     return await company_service.create_company(company_data, current_user)
+
+
+@router.post("/{company_id}/tags/create/")
+async def create_tag(tag_id: int):
+    pass
+
+
+@router.patch("/{company_id}/tags/{tag_id}/update")
+async def update_tag(tag_id: int):
+    pass
+
+
+@router.delete("/{company_id}/tags/{tag_id}/delete")
+async def delete_tag(tag_id: int):
+    pass

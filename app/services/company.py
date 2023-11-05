6e1eb409e11c9ead68e8423ec2d44819d774b848
Ilya <ilyadronov2332@gmail.com>
2023-11-05 23:44:01 +0200

    refactor: moved all tag operations into the tag service

--- app/services/company.py
+++ app/services/company.py
@@ -1,13 +1,11 @@
 from fastapi import HTTPException, status
 from sqlalchemy.exc import IntegrityError
 
-from app.models.db.users import Tag, User
+from app.models.db.users import User
 from app.models.schemas.companies import CompanyCreate, CompanyCreateSuccess
 from app.models.schemas.company_user import CompanyFullSchema
-from app.models.schemas.tags import TagBaseSchema
 from app.repository.company import CompanyRepository
 from app.utilities.formatters.http_error import validation_error_wrapper
-from app.utilities.validators.permission.user import validate_user_company_role
 
 
 class CompanyService:
@@ -42,18 +40,3 @@ class CompanyService:
 
         company = await self.company_repository.get_company_by_id(company_id)
         return CompanyFullSchema.from_model(company)
-
-    async def get_company_tags(
-        self, current_user: User, company_id: int
-    ) -> list[TagBaseSchema]:
-        if not await self.company_repository.exists_by_id(company_id):
-            raise HTTPException(
-                status.HTTP_404_NOT_FOUND, detail="Company is not found"
-            )
-
-        company = await self.company_repository.get_company_by_id(company_id)
-        if not validate_user_company_role(company, current_user):
-            raise HTTPException(status.HTTP_403_FORBIDDEN, detail="Forbidden")
-
-        tags: list[Tag] = await self.company_repository.get_company_tags(company_id)
-        return [TagBaseSchema(id=tag.id, title=tag.title) for tag in tags]
--- app/services/tag.py
+++ app/services/tag.py
@@ -1,6 +1,36 @@
+from fastapi import HTTPException, status
+
+from app.models.db.users import Tag
+from app.models.schemas.tags import TagBaseSchema, TagSchema
+from app.repository.company import CompanyMember, CompanyRepository
 from app.repository.tag import TagRepository
+from app.utilities.validators.permission.user import validate_user_company_role
 
 
 class TagService:
-    def __init__(self, tag_repository) -> None:
+    def __init__(self, tag_repository, company_repository) -> None:
         self.tag_repository: TagRepository = tag_repository
+        self.company_repository: CompanyRepository = company_repository
+
+    async def get_company_tags(
+        self, current_user_id: int, company_id: int
+    ) -> list[TagBaseSchema]:
+        if not await self.company_repository.exists_by_id(company_id):
+            raise HTTPException(
+                status.HTTP_404_NOT_FOUND, detail="Company is not found"
+            )
+
+        members: list[
+            CompanyMember
+        ] = await self.company_repository.get_company_members(company_id)
+        if not validate_user_company_role(members, current_user_id):
+            raise HTTPException(status.HTTP_403_FORBIDDEN, detail="Forbidden")
+
+        tags: list[Tag] = await self.tag_repository.get_company_tags(company_id)
+        return [TagBaseSchema(id=tag.id, title=tag.title) for tag in tags]
+
+    async def get_tag_by_id(self, tag_id: int) -> TagSchema:
+        if not self.tag_repository.exists_by_id(tag_id):
+            raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Tag is not found")
+
+        # tag = self.tag_repository.get_tag_by_id(tag_id)
